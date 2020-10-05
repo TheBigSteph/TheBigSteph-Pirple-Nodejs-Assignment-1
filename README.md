1be9151888a8e9f1b5c1dccc384aed1def854007
TheBigSteph <nzali.stephane86@gmail.com>
2020-10-05 01:52:16 +0100

    finish project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TheBigSteph-Pirple-Nodejs-Assignment-1
+
+creation of a simple "Hello World" API
+
+The RESTful JSON API listerns on port 3000
+
+When someone sends an HTTP request to the route /hello, this one receive a welcome message
--- index.js
+++ index.js
@@ -3,12 +3,76 @@
 */
 
 var http = require('http');
+const { StringDecoder } = require('string_decoder');
+var url = require('url');
+const { callbackify } = require('util');
+var stringDecoder = require('string_decoder').StringDecoder;
 
 
 var server = http.createServer(function(req, res) {
-    res.end('Hello World!');
+    // Get and parse URL
+    var parseUrl = url.parse(req.url, true);
+
+    // Get request path
+    var path = parseUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // Get the query string as an object
+    var queryStringObject = parseUrl.query;
+
+    // Get HTTP method
+    var method = req.method.toLowerCase();
+
+    // Get headers
+    var headers = req.headers;
+
+    // Get payload
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
+    req.on('data', function(data) {
+        buffer += decoder.write(data);
+    });
+
+    req.on('end', function() {
+        buffer += decoder.end();
+        var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+        
+        var data = {
+            trimmedPath: trimmedPath,
+            queryStringObject: queryStringObject,
+            method: method,
+            headers: headers,
+            payload: buffer
+        };
+
+        chosenHandler(data, function(statusCode, payload) {
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+            payload = typeof(payload) == 'object' ? payload : {};
+            var payloadString = JSON.stringify(payload);
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+            console.log('Returning this response: ', statusCode, payloadString);
+        })
+    });
+
 });
 
 server.listen(3000, function() {
     console.log('Server listerning on port 3000');
 });
+
+var handlers = {};
+// hello handler
+handlers.hello = function(data, callback) {
+    callback(401, {message: 'Greeting from a Node js lover'});
+}
+
+// not found handler
+handlers.notFound = function(data, callback) {
+    callback(404);
+}
+
+var router = {
+    hello: handlers.hello
+}
